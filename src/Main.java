54f320cceda43237d14e0eb650490869d1fc51f2
s-miehlbradt <sabine.miehlbradt@scrutinizer-ci.com>
2015-08-26 17:44:45 +0200

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,11 @@
 public class Main {
-
+ public String val1 = "CONSTANT";
+ public String val2 = "CONSTANT";
+ public String val3 = "CONSTANT";
+ 
+ public String longval1 = "ABCDEFGHIJKLMNOPQRSTUVXYZ";
+ public String longval2 = "ABCDEFGHIJKLMNOPQRSTUVXYZ";
+ public String longval3 = "ABCDEFGHIJKLMNOPQRSTUVXYZ";
     public static void main(String[] args) {
         System.out.println("Hello World!");
     }
