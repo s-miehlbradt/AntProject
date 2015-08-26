2028f3f132a891f2059e9ddb5f662a828ca7d9e5
s-miehlbradt <sabine.miehlbradt@scrutinizer-ci.com>
2015-08-26 17:50:36 +0200

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,29 @@
 public class Main {
- public String val1 = "CONSTANT";
- public String val2 = "CONSTANT";
- public String val3 = "CONSTANT";
+ public String val01 = "CONSTANT";
+ public String val02 = "CONSTANT";
+ public String val03 = "CONSTANT";
+ public String val04 = "CONSTANT";
+ public String val05 = "CONSTANT";
+ public String val06 = "CONSTANT";
+ public String val07 = "CONSTANT";
+ public String val08 = "CONSTANT";
+ public String val09 = "CONSTANT";
+ public String val10 = "CONSTANT";
+ public String val11 = "CONSTANT";
+ public String val12 = "CONSTANT";
+ public String val13 = "CONSTANT";
+ public String val15 = "CONSTANT";
+ public String val16 = "CONSTANT";
+ public String val17 = "CONSTANT";
+ public String val18 = "CONSTANT";
+ public String val19 = "CONSTANT";
+ public String val20 = "CONSTANT";
+ public String val20 = "CONSTANT";
  
- public String longval1 = "ABCDEFGHIJKLMNOPQRSTUVXYZ";
- public String longval2 = "ABCDEFGHIJKLMNOPQRSTUVXYZ";
- public String longval3 = "ABCDEFGHIJKLMNOPQRSTUVXYZ";
+ 
+ public String longval1 = "ABCDEFGHIJKLMNOPQRSTUVXYZABCDEFGHIJKLMNOPQRSTUVXYZ";
+ public String longval2 = "ABCDEFGHIJKLMNOPQRSTUVXYZABCDEFGHIJKLMNOPQRSTUVXYZ";
+ public String longval3 = "ABCDEFGHIJKLMNOPQRSTUVXYZABCDEFGHIJKLMNOPQRSTUVXYZ";
     public static void main(String[] args) {
         System.out.println("Hello World!");
     }
